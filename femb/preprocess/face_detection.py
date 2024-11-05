8fdaed1df690cc5dfdef7dedc762ba3ff514e785
yunseobyuk <ys.yuk@gmail.com>
2024-11-05 08:23:50 +0000

    change face_detion to detect one face

--- femb/preprocess/face_detection.py
+++ femb/preprocess/face_detection.py
@@ -24,32 +24,39 @@ def preprocess_image(img, size=224):
     tensor_imgs = []
     byte_buffers = []
 
+    
     # 1-1. 얼굴 검출 결과 표시
     if boxes is not None:
         boxed_img = img.copy()
-        for box in boxes:
-            # box는 [x1, y1, x2, y2] 형식의 좌표를 가진다.
-            x1, y1, x2, y2 = map(int, box)
-            # 음수 좌표가 있을 경우 0으로 변경
-            x1, y1 = max(0, x1), max(0, y1)
-            x2, y2 = min(img.shape[1], x2), min(img.shape[0], y2)
-            # cv2로 사각형 그리기 (BGR 형식으로 빨간색 사용)
-            cv2.rectangle(boxed_img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-            # 얼굴영역 cropping
-            roi = img[y1:y2, x1:x2]
-            # 2. resizing
-            resized_img = cv2.resize(roi, (size,size))
-            # 3. Normalization
-            normalized_image = resized_img / 255.0
-            # 4. Transforms to tensor -> bytebuffer
-            to_tensor = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            tensor_img = to_tensor(normalized_image)
-            byte_buffer = tensor_img.numpy().tobytes()
+        h, w, _ = img.shape
+        # 이미지의 중심점 계산
+        img_center = np.array([w / 2, h / 2])
 
-            tensor_imgs.append(tensor_img)
-            byte_buffers.append(byte_buffer)
+        # 얼굴 중심점 계산 및 중앙과의 거리 계산
+        closest_box = min(boxes, key=lambda box: np.linalg.norm(img_center - np.array([(box[0] + box[2]) / 2, (box[1] + box[3]) / 2])))
+
+        # box는 [x1, y1, x2, y2] 형식의 좌표를 가진다.
+        x1, y1, x2, y2 = map(int, closest_box)
+        # 음수 좌표가 있을 경우 0으로 변경
+        x1, y1 = max(0, x1), max(0, y1)
+        x2, y2 = min(img.shape[1], x2), min(img.shape[0], y2)
+        # cv2로 사각형 그리기 (BGR 형식으로 빨간색 사용)
+        cv2.rectangle(boxed_img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        # 얼굴영역 cropping
+        roi = img[y1:y2, x1:x2]
+        # 2. resizing
+        resized_img = cv2.resize(roi, (size,size))
+        # 3. Normalization
+        normalized_image = resized_img / 255.0
+        # 4. Transforms to tensor -> bytebuffer
+        to_tensor = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        tensor_img = to_tensor(normalized_image)
+        byte_buffer = tensor_img.numpy().tobytes()
+
+        tensor_imgs.append(tensor_img)
+        byte_buffers.append(byte_buffer)
 
         for tensor_img in tensor_imgs:
             visualized_img = tensor_img.permute(1,2,0)
